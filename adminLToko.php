b6080a76b43167ecdc1ff09972e17f916e590166
Dio Antares <dioantares@gmail.com>
2020-05-07 23:22:42 +0700

    Update Admin

---
+++ adminLToko.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>index</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/Data-Table-1.css">
+    <link rel="stylesheet" href="assets/css/Data-Table.css">
+    <link rel="stylesheet" href="assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
+    <link rel="stylesheet" href="assets/css/LinkedIn-like-Profile-Box.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Table-With-Search-1.css">
+    <link rel="stylesheet" href="assets/css/Table-With-Search.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md" style="background-color: #ffffff;">
+        <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><img src="assets/img/Gipay.png" style="height: 40px;">
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul>
+            </div><input class="form-control-plaintext" type="text" value="Welcome, dioantares" readonly="" style="width: 205px;font-size: 18px;"><a href="index.html" class="btn btn-primary" role="button">Sign Out</a></div>
+    </nav>
+    <nav class="navbar navbar-light navbar-expand-md">
+        <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-2"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-2" style="background-color: #d7d6d6;font-size: 20px;height: 50px;color: rgb(0,0,0);">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul><a href="" style="color: rgb(0,0,0);width: 210px;">&nbsp;List Pengguna Publik</a><a href="" style="color: rgb(0,0,0);width: 169px;">List Pemilik Toko</a><a href="" style="color: rgb(0,0,0);width: 180px;">Verifikasi TopUp</a><a href=""
+                    style="color: rgb(0,0,0);width: 215px;">Presentase Potongan</a><a href="" style="color: rgb(0,0,0);">Statistik SignUp</a></div>
+        </div>
+    </nav>
+    <nav class="navbar navbar-light navbar-expand-md" style="background-color: #ffffff;">
+        <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul>
+            </div><form class="form-blokPub" action="" method="POST"><input type="text" style="margin-right: 8px;"><a href="" class="btn btn-primary" role="button">Delete</a></form></div>
+    </nav><table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>idUser</th>
+                <th>Nama</th>
+                <th>Nama Toko</th>
+                <th>Alamat Toko</th>
+                <th>Email</th>
+                <th>No HP</th>
+                <th>Saldo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>rio</td>
+                <td>rio</td>
+                <td>rio</td>
+                <td>rio</td>
+                <td>rio</td>
+                <td>rio</td>
+                <td>rio</td>
+            </tr>
+        </tbody>
+    </table>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <script src="assets/js/Table-With-Search.js"></script>
+</body>
+
+</html>
