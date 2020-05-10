3f1e3f28098317bc109c7fe2d0da0d8afa1cf46a
YazooKazoo <30350331+YazooKazoo@users.noreply.github.com>
2020-05-10 18:20:07 +0700

    Update DB

--- gipay.sql
+++ gipay.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2020 at 12:49 PM
+-- Generation Time: May 10, 2020 at 01:19 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
